391ecd836351e76656dc321157fe1e0f1ea58065
dejanastevic2002 <dejana.stevic.23@singimail.rs>
2026-09-07 13:47:27 +0200

    Dodat frontend u projekat

---
+++ src/hooks/useListData.js
@@ -0,0 +1,20 @@
+import {useCallback, useState} from "react";
+import axios from "axios";
+
+const useListData=(url) => {
+    const[loading,setLoading] =useState(false);
+    const[data,setData]=useState({});
+
+    const getData=useCallback(async (url) =>{
+        setLoading(true);
+
+        let result=await axios.get(url);
+
+        setData(result.data);
+        setLoading(false);
+
+    }, [url]);
+    return {getData,loading,data};
+
+}
+export default useListData;
